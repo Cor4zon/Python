96f13a90ccf44ed7eab786c252715878e8a79dfb
Victor Fedorov <vpfedorov@bk.ru>
2021-08-18 21:16:19 +0300

    great default choice for queue

---
+++ DataStructures/Queue/dequeQueue.py
@@ -0,0 +1,26 @@
+# great default choice
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, item):
+        self.queue.append(item)
+
+    def pop(self):
+        if len(self.queue) == 0:
+            return None
+        return self.queue.popleft()
+
+
+q = Queue()
+
+q.push(1)
+q.push(2)
+q.push(3)
+
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())
