a1132f2e2674b5acf37bc668f7e0fd7585c93b7a
Victor Fedorov <vpfedorov@bk.ru>
2021-11-05 19:07:08 +0300

    dunder methods for Stack

--- DataStructures/Stack/myStack.py
+++ DataStructures/Stack/myStack.py
@@ -3,12 +3,21 @@ class Stack:
     def __init__(self):
         self.stack = []
 
+    def __iter__(self):
+        for elem in self.stack:
+            yield elem
+
+    def __len__(self):
+        counter = 0
+        for elem in self.stack:
+            counter += 1
+        return counter
+
     def pop(self):
         if len(self.stack) == 0:
             print("Stack underflow")
             return None
         return self.stack.pop()
-
 
     def push(self, item):
         self.stack.append(item)
@@ -18,10 +27,13 @@ myStack = Stack()
 myStack.push(1)
 myStack.push(2)
 myStack.push(3)
-
-print(myStack.stack)
+print(len(myStack))
+for i in myStack:
+    print(i, end=" ")
+print()
 
 print(myStack.pop())
 print(myStack.pop())
 print(myStack.pop())
 myStack.pop()
+
